94bdc584dff7c296468fad3f462cb58bdb34025c
mark baruch <mbaruch28@gmail.com>
2019-02-12 16:03:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,15 +2,3 @@ def oxford_comma(array)
 choclate = "kit kat","twist","hersheys kisses","crunch"]
 heavenly = chocolate.join(",")
 end
-
-do
-end
- choclate = "kit kat","twist","hersheys kisses","crunch"]
-heavenly = chocolate.join(",")
-
-
-
-
-="ararys into strings"
-split
-join
